800f95bde85de160d818b8186a21d2dff7180a43
harshadapawarsnowflake <109716899+harshadapawarsnowflake@users.noreply.github.com>
2022-07-21 17:43:07 +0530

    Update streamlit_app.py
    import requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 streamlit.text(fruityvice_response)
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -14,7 +15,6 @@ fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.i
 #streamlit.dataframe(my_fruit_list) 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show) 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 
