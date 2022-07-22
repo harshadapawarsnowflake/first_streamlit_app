21e620e2f136a729fd958f2a31eb9634197d0fa9
harshadapawarsnowflake <109716899+harshadapawarsnowflake@users.noreply.github.com>
2022-07-22 23:58:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 #streamlit.dataframe(my_data_row)
 def get_fruit_load_list():
-      with  my_cnx_cursor as  my_cur
+      with  my_cnx_cursor as  my_cur:
             my_cur.execute("select * from fruit_load_list")
             return my_cur.fetchall()
 if streamlit.button('Get Fruit Load List'):
