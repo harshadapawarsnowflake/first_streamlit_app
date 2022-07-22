2b865c047b60b28046a11f234ab2fabe5442e607
harshadapawarsnowflake <109716899+harshadapawarsnowflake@users.noreply.github.com>
2022-07-22 21:49:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
